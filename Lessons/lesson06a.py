696fa0cfa6234628414b17171816a97dc165898a
Demetrio de Paula <demetrioap@users.noreply.github.com>
2020-07-12 21:21:31 -0400

    Lesson 06a
    This is the first one with Python code.
    The previous ones where initial explanations and PyCharm installation.

---
+++ Lessons/lesson06a.py
@@ -0,0 +1,33 @@
+# Curso em Vídeo Lesson 06
+# Primitive Types and Data Output
+# https://www.youtube.com/watch?v=hdDHg1p3YVc&list=PLHz_AreHm4dlKP6QQCekuIPky1CiwmdI6&index=9
+
+n1 = int(input('Type the first number: '))
+n2 = int(input('Type the second number: '))
+s = n1 + n2
+print('Result is', s)
+
+print('The sum is {}'.format(s))
+
+###
+
+n1 = int(input('Type a number '))
+print(type(n1))
+
+n2 = int(input('Type another number '))
+print(type(n2))
+
+s = n1 + n2
+
+print('Result is: {}'.format(s))
+
+# Output options
+
+# my way before teacher's resolution
+print('The sum of {}'.format(n1) + ' and {}'.format(n2) + ' is: {}'.format(s))
+
+# teacher's solution
+print('The sum of {} and {} is {}'.format(n1, n2, s))
+
+# another option, printing via index
+print('The sum of {0} and {1} is {2}'.format(n1, n2, s))
