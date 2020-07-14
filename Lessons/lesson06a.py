ecca7b40fff6f5fa57fcdcd1a469ad6a7c187ac7
Demetrio de Paula <demetrioap@users.noreply.github.com>
2020-07-13 20:46:16 -0400

    Edited comments.

--- Lessons/lesson06a.py
+++ Lessons/lesson06a.py
@@ -5,13 +5,18 @@
 n1 = int(input('Type the first number: '))
 n2 = int(input('Type the second number: '))
 s = n1 + n2
+
+# regular print.
 print('Result is', s)
 
+# print using format().
 print('The sum is {}'.format(s))
 
-###
 
-n1 = int(input('Type a number '))
+# Finding the type using type()
+
+n1 = int(input('Type a number: '))
+print('The type of the number is:')
 print(type(n1))
 
 n2 = int(input('Type another number '))
@@ -23,11 +28,11 @@ print('Result is: {}'.format(s))
 
 # Output options
 
-# my way before teacher's resolution
+# my way before teacher's resolution.
 print('The sum of {}'.format(n1) + ' and {}'.format(n2) + ' is: {}'.format(s))
 
-# teacher's solution
+# teacher's solution.
 print('The sum of {} and {} is {}'.format(n1, n2, s))
 
-# another option, printing via index
+# another option, printing via index.
 print('The sum of {0} and {1} is {2}'.format(n1, n2, s))
