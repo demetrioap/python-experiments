866458de54fc5d4e0e75cc104ffeb005ff0b9a29
Demetrio de Paula <demetrioap@users.noreply.github.com>
2020-07-19 10:29:49 -0400

    Create lesson08b.py
    Trying libraries

---
+++ Lessons/lesson08b.py
@@ -0,0 +1,18 @@
+# Curso em Vídeo Lesson 08
+# https://www.youtube.com/watch?v=oOUyhGNib2Q&list=PLHz_AreHm4dlKP6QQCekuIPky1CiwmdI6&index=24
+
+# Trying libraries
+
+import random
+import emoji
+
+# Random
+num = random.random()
+print(num)
+
+num = random.randint(1, 10)
+print(num)
+
+
+# Emoji
+print(emoji.emojize('Hello World! :earth_americas:', use_aliases=True))
