ffce931742da19980b76737216b2d5b086fdbc21
Demetrio de Paula <demetrioap@users.noreply.github.com>
2020-07-28 20:37:14 -0400

    Curso em Vídeo Lesson 12 Nested Conditions

---
+++ Lessons/lesson12.py
@@ -0,0 +1,13 @@
+# Curso em Vídeo Lesson 12
+# Nested Conditions
+# https://www.youtube.com/watch?v=j9bYDjaAYzw&list=PLHz_AreHm4dk_nZHmxxf_J0WRAqy5Czye&index=2
+
+name = str(input('Type a name: ')).strip()
+
+if name == 'Pedro':
+    print('Menu mon!')
+elif name == 'Jane':
+    print('Mô bem!')
+else:
+    print('Ok, maca.')
+print('Have a normal day.')
