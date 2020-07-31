06a3fc7fb3183878279d14af4e188a3033d40b4c
Demetrio de Paula <demetrioap@users.noreply.github.com>
2020-07-30 20:59:17 -0400

    Files renamed.

--- Challenges/challenge001 - lesson 4.py
+++
@@ -1,2 +0,0 @@
-name = input('Type your name')
-print('Hi, ' + name + '! Nice to meet you')
--- Challenges/challenge002 - lesson 4.py
+++
@@ -1,7 +0,0 @@
-# Challenge 02 - Lesson 04
-
-print('Tell me your date of birth')
-day = input('What is the day? ')
-month = input('Which month? ')
-year = input('And the year? ')
-print('You were born on day', day, 'of month', month, 'of', year, 'correct?')
